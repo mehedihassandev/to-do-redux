eabe2df5632b5606f3f89e5c087d90d7ce0bc537
Md. Mehedi Hassan <59695824+mehedihassandev@users.noreply.github.com>
2023-08-14 18:44:24 +0600

    done

---
+++ src/redux/todo/initialState.js
@@ -0,0 +1,15 @@
+export const initialState = [
+    {
+        id: 1,
+        text: "Learn React",
+        completed: true
+    },
+
+    {
+        id: 2,
+        text: "Learn Redux",
+        completed: false,
+        color: "green",
+    }
+    
+]
